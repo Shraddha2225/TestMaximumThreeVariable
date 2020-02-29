904b9bdac6a7cefafc9297b5e75ecf2bba3f56c1
Shraddha2225 <shraddhat22m@gmail.com>
2020-02-29 10:58:58 +0530

    ADDED: Print Maximum Using Generic Method

--- src/main/java/com/demo/bridgelab/FindMaximumNumber.java
+++ src/main/java/com/demo/bridgelab/FindMaximumNumber.java
@@ -1,4 +1,7 @@
 package com.demo.bridgelab;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class FindMaximumNumber <E extends Comparable <E>>{
     E i1;
     E i2;
@@ -11,7 +14,7 @@ public class FindMaximumNumber <E extends Comparable <E>>{
     public E ChecktestMaximumNumber(){
         return ChecktestMaximumNumber(i1,i2,i3);
     }
-    public static <E extends Comparable<E>> E ChecktestMaximumNumber(E i1, E i2, E i3){
+    public static <E extends Comparable<E>> E ChecktestMaximumNumber(E i1, E i2, E i3, E...optionalParameter){
             E max = i1;
             if (i2.compareTo(max) > 0){
                 max = i2;
@@ -19,6 +22,17 @@ public class FindMaximumNumber <E extends Comparable <E>>{
             if(i3.compareTo(max) > 0) {
                 max = i3;
             }
+            if (optionalParameter.length != 0){
+                Arrays.sort(optionalParameter, Collections.reverseOrder());
+                if (optionalParameter[0].compareTo(max) > 0){
+                    max = optionalParameter[0];
+                }
+            }
+            printMaximum(max);
             return max;
+    }
+
+    private static <E extends Comparable<E>> void printMaximum(E max) {
+        System.out.println("Maximum Variable Is :" +max);
     }
 }
--- src/test/java/com/demo/bridgelab/TestMaximumNumber.java
+++ src/test/java/com/demo/bridgelab/TestMaximumNumber.java
@@ -61,5 +61,22 @@ public class TestMaximumNumber {
         String maximumString = (String) string.ChecktestMaximumNumber();
         Assert.assertEquals("Peach",maximumString);
     }
+    @Test
+    public void givenIntegerNumberOptionalArgument_WhenProper_ReturnMaximumNumber() {
+        Integer maximumNumber = (Integer) FindMaximumNumber.ChecktestMaximumNumber(1,2,3,4,5,6);
+        Assert.assertEquals((Integer) 6,maximumNumber);
+    }
 
+    @Test
+    public void givenFloatNumberOptionalArgument_WhenProper_ReturnMaximumNumber() {
+        Float maximumNumber = (Float) FindMaximumNumber.ChecktestMaximumNumber(1.1f, 2.2f, 3.4f, 4.7f, 7.8f, 18.2f);
+        Assert.assertEquals(18.2f,maximumNumber,0.00);
+    }
+
+    @Test
+    public void givenStringOptionalArgument_WhenProper_ReturnMaximunString() {
+        FindMaximumNumber string = new FindMaximumNumber("Banana","Apple","Peach");
+        String maximumString = (String) FindMaximumNumber.ChecktestMaximumNumber("Banana","Apple","Peach","Mango","Grapes","StrawBerry");
+        Assert.assertEquals("StrawBerry",maximumString);
+    }
 }

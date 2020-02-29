9576d9f0663b977e5ec48e514e4170b7ffd3bc96
Shraddha2225 <shraddhat22m@gmail.com>
2020-02-29 09:35:27 +0530

    REFACTORED:Create Generic Class To Take In 3 Varible Of Generic Type

--- src/main/java/com/demo/bridgelab/FindMaximumNumber.java
+++ src/main/java/com/demo/bridgelab/FindMaximumNumber.java
@@ -1,6 +1,17 @@
 package com.demo.bridgelab;
-public class FindMaximumNumber {
-    public <E extends Comparable<E>> E TestMaximumNumber(E i1, E i2, E i3){
+public class FindMaximumNumber <E extends Comparable <E>>{
+    E i1;
+    E i2;
+    E i3;
+    public FindMaximumNumber(E i1, E i2, E i3){
+        this.i1 = i1;
+        this.i2 = i2;
+        this.i3 = i3;
+    }
+    public E ChecktestMaximumNumber(){
+        return ChecktestMaximumNumber(i1,i2,i3);
+    }
+    public static <E extends Comparable<E>> E ChecktestMaximumNumber(E i1, E i2, E i3){
             E max = i1;
             if (i2.compareTo(max) > 0){
                 max = i2;
@@ -10,5 +21,4 @@ public class FindMaximumNumber {
             }
             return max;
     }
-
 }
--- src/test/java/com/demo/bridgelab/TestMaximumNumber.java
+++ src/test/java/com/demo/bridgelab/TestMaximumNumber.java
@@ -5,60 +5,60 @@ import org.junit.Test;
 public class TestMaximumNumber {
     @Test
     public void givenIntegerNumber_WhenAtFirstPosition_ReturnMaximumNumber() {
-        FindMaximumNumber number = new FindMaximumNumber();
-        int maximumValue = number.TestMaximumNumber(3,2,1);
-        Assert.assertEquals(3,maximumValue);
+        FindMaximumNumber number = new FindMaximumNumber(3,2,1);
+        Integer maximumValue = (Integer) number.ChecktestMaximumNumber();
+        Assert.assertEquals((Integer) 3,maximumValue);
     }
     @Test
     public void givenIntegerNumber_WhenAtSecondPosition_ReturnMaximumNumber() {
-        FindMaximumNumber number = new FindMaximumNumber();
-        int maximumValue = number.TestMaximumNumber(2,3,1);
-        Assert.assertEquals(3,maximumValue);
+        FindMaximumNumber number = new FindMaximumNumber(2,3,1);
+        Integer maximumValue = (Integer)number.ChecktestMaximumNumber();
+        Assert.assertEquals((Integer)3,maximumValue);
     }
     @Test
     public void givenIntegerNumber_WhenAtThirdPosition_ReturnMaximumNumber() {
-        FindMaximumNumber number = new FindMaximumNumber();
-        int maximumValue = number.TestMaximumNumber(1,2,3);
-        Assert.assertEquals(3,maximumValue);
+        FindMaximumNumber number = new FindMaximumNumber(1,2,3);
+        Integer maximumValue = (Integer)number.ChecktestMaximumNumber();
+        Assert.assertEquals((Integer)3,maximumValue);
     }
     @Test
     public void givenFloatNumber_WhenAtFirstPosition_ReturnMaximumNumber() {
-        FindMaximumNumber number = new FindMaximumNumber();
-        Double maximumNumber = number.TestMaximumNumber(3.3,2.2,1.1);
-        Assert.assertEquals(3.3,maximumNumber,0.00);
+        FindMaximumNumber number = new FindMaximumNumber(3.3f,2.2f,1.1f);
+        Float maximumNumber = (Float) number.ChecktestMaximumNumber();
+        Assert.assertEquals(3.3f,maximumNumber,0.00);
     }
 
     @Test
     public void givenFloatNumber_WhenAtSecondPosition_ReturnMaximumNumber() {
-        FindMaximumNumber number = new FindMaximumNumber();
-        Double maximumNumber = number.TestMaximumNumber(2.2,3.3,1.1);
-        Assert.assertEquals(3.3,maximumNumber,0.00);
+        FindMaximumNumber number = new FindMaximumNumber(2.2f,3.3f,1.1f);
+        Float maximumNumber = (Float) number.ChecktestMaximumNumber();
+        Assert.assertEquals(3.3f,maximumNumber,0.00);
     }
 
     @Test
     public void givenFloatNumber_WhenAtThirdPosition_ReturnMaximumNumber() {
-        FindMaximumNumber number = new FindMaximumNumber();
-        Double maximumNumber = number.TestMaximumNumber(1.1,2.2,3.3);
-        Assert.assertEquals(3.3,maximumNumber,0.00);
+        FindMaximumNumber number = new FindMaximumNumber(2.2f,1.1f,3.3f);
+        Float maximumNumber = (Float) number.ChecktestMaximumNumber();
+        Assert.assertEquals(3.3f,maximumNumber,0.00);
     }
     @Test
     public void givenString_WhenAtFirstPosition_ReturnMaximumString() {
-        FindMaximumNumber string = new FindMaximumNumber();
-        String maximumString = string.TestMaximumNumber("Peach","Banana","Apple");
+        FindMaximumNumber string = new FindMaximumNumber("Peach","Banana","Apple");
+        String maximumString = (String) string.ChecktestMaximumNumber();
         Assert.assertEquals("Peach",maximumString);
     }
 
     @Test
     public void givenString_WhenAtSecondPosition_ReturnMaximumString() {
-        FindMaximumNumber string = new FindMaximumNumber();
-        String maximumString = string.TestMaximumNumber("Apple","Peach","Banana");
+        FindMaximumNumber string = new FindMaximumNumber("Banana","Peach","Apple");
+        String maximumString = (String) string.ChecktestMaximumNumber();
         Assert.assertEquals("Peach",maximumString);
     }
 
     @Test
     public void givenString_WhenAtThirdPosition_ReturnMaximumString() {
-        FindMaximumNumber string = new FindMaximumNumber();
-        String maximumString = string.TestMaximumNumber("Apple","Banana","Peach");
+        FindMaximumNumber string = new FindMaximumNumber("Banana","Apple","Peach");
+        String maximumString = (String) string.ChecktestMaximumNumber();
         Assert.assertEquals("Peach",maximumString);
     }
 
